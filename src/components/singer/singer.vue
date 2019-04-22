c347dcdbeb4f3df10971e146dfc55746c5788435
fengmiao@echinacoop.com <fengmiao@echinacoop.com>
2019-04-22 22:00:58 +0800

    歌单列表

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="singer">
-    
+    <listView :data="singers"></listView>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
 import {getSingerList} from "../../api/singer.js"
 import { ERR_OK } from "../../api/config.js";
+import Singer from "common/js/singer";
+import listView from "../../base/listview/listview"
+  const HOT_NAME = '热门'
+  const HOT_SINGER_LEN= 10
   export default {
     data(){
       return{
@@ -18,14 +22,59 @@ import { ERR_OK } from "../../api/config.js";
     },
     methods: {
       _getsingerList() {
-      getSingerList().then(res => {
-        if (res.code === ERR_OK) {
-          this.singers = res.data.list;
-          console.log(this.singers)
+        getSingerList().then(res => {
+          if (res.code === ERR_OK) {
+            this.singers = this._nomalizeSinger(res.data.list);
+          }
+        });
+      },
+      _nomalizeSinger(list) {
+        let map ={
+          hot: {
+            title:HOT_NAME,
+            items:[]
+          }
         }
-      });
+        list.forEach((item,index) => {
+          if(index<HOT_SINGER_LEN){
+            map.hot.items.push(new Singer({
+              id: item.Fsinger_mid,
+              name: item.Fsinger_name, 
+            }))
+          }
+          const key = item.Findex
+          if(!map[key]){
+            map[key] ={
+              title:key,
+              items:[]
+            }
+          }
+          map[key].items.push(new Singer({
+              id: item.Fsinger_mid,
+              name: item.Fsinger_name, 
+            }))
+        
+        });
+        //为了得到有序列表，处理map
+        let hot=[]
+        let ret=[]
+        for(let key in map){
+          let val = map[key]
+          if(val.title.match(/[a-zA-Z]/)){
+            ret.push(val)
+          }else if(val.title==='HOT_NAME'){
+            hot.push(val)
+          }
+        }
+        ret.sort((a,b)=>{
+          return a.title.charCodeAt(0)-b.title.charCodeAt(0)
+        })
+        return hot.concat(ret)
+      }
     },
-    },
+    components:{
+      listView
+    }
   }
 </script>
 
